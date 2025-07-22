dcabec35e793ba2ae7c2c845ba14f1500afc17b4
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-07-22 17:59:29 -0500

    Added new example: LED_Binary

---
+++ led_binary/code/led_binary.ino
@@ -0,0 +1,40 @@
+#define DELAY_MS 500
+#define PIN0 8 // 0 = off
+#define PIN1 9 // 1 = on
+
+void displayBinaryToLEDs(char *binary) {
+  for (int i = 0; i < strlen(binary); i++) {
+    if (binary[i] == '1') {
+      digitalWrite(PIN1, HIGH);
+      delay(DELAY_MS);
+      digitalWrite(PIN1, LOW);
+    }    
+
+    else {
+      digitalWrite(PIN0, HIGH);
+      delay(DELAY_MS);
+      digitalWrite(PIN0, LOW);
+    }
+
+    digitalWrite(PIN0, LOW);
+    digitalWrite(PIN1, LOW);
+
+    delay(DELAY_MS);
+  }
+}
+
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+  char binary[17];
+
+  for (int i = 0; i <= 15; i++) {
+    itoa(i, binary, 2);
+    displayBinaryToLEDs(binary);
+
+    delay(DELAY_MS * 2); // To separate each number
+  }
+}
